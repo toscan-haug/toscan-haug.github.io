89e198a8384bad70e47ff3b28d2ffc8d285b317b
Christophe Haug <christophe.haug@m4x.org>
2022-03-13 14:08:28 +0100

    split App

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Link, Outlet
-} from "react-router-dom";
+import App from './components/App';
 
 import './index.scss';
-
-import HomePage from './pages/Home';
-import MusicPage from './pages/Music';
-import VideoPage from './pages/Video';
-import DrawingPage from './pages/Drawing';
-
-const App = () => {
-  return <Router>
-    <Routes>
-      <Route path="/"         element={<Layout />}>
-        <Route index          element={<HomePage />} />
-        <Route path="music"   element={<MusicPage />} />
-        <Route path="video"   element={<VideoPage />} />
-        <Route path="drawing" element={<DrawingPage />} />
-      </Route>
-    </Routes>
-  </Router>
-}
-
-const Layout = () => {
-  return <div className="portfolio">
-    <nav>
-      <Link to="/"        >Home</Link> |{" "}
-      <Link to="/music"   >Musique</Link> |{" "}
-      <Link to="/video"   >Vidéo</Link> |{" "}
-      <Link to="/drawing" >Animation</Link>
-    </nav>
-    <Outlet/>
-  </div>
-}
 
 ReactDOM.render(
   <React.StrictMode>
